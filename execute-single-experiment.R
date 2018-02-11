0faeee5496ea92f2d792e98930b115a3461d8f01
kclary <kclary@cs.umass.edu>
2018-02-11 04:30:11 -0500

    Updated configs read location

--- execute-single-experiment.R
+++ execute-single-experiment.R
@@ -3,5 +3,5 @@ args = commandArgs(trailingOnly=TRUE)
 
 source("test-suite.R")
 
-configs <- read.csv("all_adv_configurations.csv")
+configs <- read.csv("configs/all_adv_configurations.csv")
 test.single.config(args[[1]], configs, trials=100, all=FALSE)
