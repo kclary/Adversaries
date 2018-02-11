46f9e04a805feb5e44c6e9874c847e8e695c1c99
Kaleigh Clary <kclary@swarm2.cm.cluster>
2018-02-11 01:27:58 -0500

    Updated number of trials to 10

--- rw-execute-single-experiment.R
+++ rw-execute-single-experiment.R
@@ -4,4 +4,4 @@ args = commandArgs(trailingOnly=TRUE)
 source("test-suite.R")
 
 configs <- read.csv("all_adv_configurations_rw.csv")
-test.single.config(args[[1]], configs, trials=100, all=FALSE)
+test.single.config(args[[1]], configs, trials=10, all=FALSE)
